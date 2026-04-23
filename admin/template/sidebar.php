1448f04abd9f8e17fdc28efdf455e47c9272f866
risman <sambeigarisman@gmail.com>
2026-04-23 10:15:08 +0700

    ok

--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -1,7 +1,8 @@
 <!--sidebar start-->
 <?php
     $id = $_SESSION['user']['id_user'];
-    $hasil_profil = $lihat -> member_edit($id);
+    $role = $_SESSION['user']['role'];
+    $hasil_profil = $lihat->member_edit($id);
 ?>
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
@@ -11,106 +12,154 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-cash-register"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Sata Priori<sup></sup></div>
+        <div class="sidebar-brand-text mx-3">Sata Priori</div>
     </a>
 
-    <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
+    <!-- Dashboard -->
     <li class="nav-item active">
         <a class="nav-link" href="index.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <span>Dashboard</span>
+        </a>
     </li>
 
-    <!-- Divider -->
     <hr class="sidebar-divider">
-    <!-- Heading -->
-    <!-- <div class="sidebar-heading">
-           Master
-       </div> -->
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item active">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-database"></i>
-            <span>Data Master</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <!-- <h6 class="collapse-header">Custom Components:</h6> -->
-                <a class="collapse-item" href="index.php?page=kategori">Kategori Menu</a>
-                <a class="collapse-item" href="index.php?page=menu">Kelola Menu</a>
-                <!-- <a class="collapse-item" href="index.php?page=user">User</a> -->
+
+    <?php if($role === 'admin'): ?>
+
+        <!-- ADMIN MENU -->
+
+        <!-- Data Master -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDataMaster"
+                aria-expanded="false" aria-controls="collapseDataMaster">
+                <i class="fas fa-fw fa-database"></i>
+                <span>Data Master</span>
+            </a>
+            <div id="collapseDataMaster" class="collapse" aria-labelledby="headingDataMaster" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="index.php?page=kategori">Kategori Menu</a>
+                    <a class="collapse-item" href="index.php?page=menu">Kelola Menu</a>
+                </div>
             </div>
-        </div>
-    </li>
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item active">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
-            aria-controls="collapse3">
-            <i class="fas fa-fw fa-desktop"></i>
-            <span>Transaksi</span>
-        </a>
-        <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <!-- <h6 class="collapse-header">Custom Components:</h6> -->
-                <a class="collapse-item" href="index.php?page=jual">Transaksi Jual</a>
-                <a class="collapse-item" href="index.php?page=laporan">Laporan Penjualan</a>
+        </li>
+
+        <!-- Data Penjualan -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePenjualan"
+                aria-expanded="false" aria-controls="collapsePenjualan">
+                <i class="fas fa-fw fa-shopping-cart"></i>
+                <span>Penjualan</span>
+            </a>
+            <div id="collapsePenjualan" class="collapse" aria-labelledby="headingPenjualan" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="index.php?page=penjualan">Data Penjualan</a>
+                    <a class="collapse-item" href="index.php?page=penjualan/import">Import Transaksi</a>
+                </div>
             </div>
-        </div>
-    </li>
-    <hr class="sidebar-divider">
-    <li class="nav-item active">
-        <a class="nav-link" href="index.php?page=pengaturan">
-            <i class="fas fa-fw fa-cogs"></i>
-            <span>Pengaturan Toko</span></a>
-    </li>
-    <!-- Divider -->
+        </li>
+
+        <!-- Apriori -->
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?page=apriori">
+                <i class="fas fa-fw fa-brain"></i>
+                <span>Proses Apriori</span>
+            </a>
+        </li>
+
+        <!-- Laporan -->
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?page=laporan">
+                <i class="fas fa-fw fa-chart-bar"></i>
+                <span>Laporan</span>
+            </a>
+        </li>
+
+        <hr class="sidebar-divider">
+
+        <!-- Pengaturan -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePengaturan"
+                aria-expanded="false" aria-controls="collapsePengaturan">
+                <i class="fas fa-fw fa-cogs"></i>
+                <span>Pengaturan</span>
+            </a>
+            <div id="collapsePengaturan" class="collapse" aria-labelledby="headingPengaturan" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="index.php?page=pengaturan">Profil Toko</a>
+                    <a class="collapse-item" href="index.php?page=user">Kelola User</a>
+                </div>
+            </div>
+        </li>
+
+    <?php elseif($role === 'kasir'): ?>
+
+        <!-- KASIR MENU -->
+
+        <!-- Transaksi -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTransaksi"
+                aria-expanded="false" aria-controls="collapseTransaksi">
+                <i class="fas fa-fw fa-desktop"></i>
+                <span>Transaksi</span>
+            </a>
+            <div id="collapseTransaksi" class="collapse" aria-labelledby="headingTransaksi" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="index.php?page=transaksi">Input Transaksi</a>
+                    <a class="collapse-item" href="index.php?page=transaksi/riwayat">Riwayat Transaksi</a>
+                </div>
+            </div>
+        </li>
+
+    <?php endif; ?>
+
     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
 
 </ul>
 <!-- End of Sidebar -->
+
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
-
-    <!-- Main Content -->
     <div id="content">
 
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-            <!-- Sidebar Toggle (Topbar) -->
             <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                 <i class="fa fa-bars"></i>
             </button>
-            <h5 class="d-lg-block d-none mt-2"><b><?php echo $toko['nama_toko'];?>, <?php echo $toko['alamat_toko'];?></b></h5>
-            <!-- Topbar Navbar -->
+
+            <h5 class="d-lg-block d-none mt-2">
+                <b><?php echo htmlspecialchars($toko['nama_toko'] ?? ''); ?>,
+                   <?php echo htmlspecialchars($toko['alamat_toko'] ?? ''); ?></b>
+            </h5>
+
             <ul class="navbar-nav ml-auto">
-                <!-- <div class="topbar-divider d-none d-sm-block"></div> -->
-                <!-- Topbar Search -->
-                <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img class="img-profile rounded-circle"
-                            src="assets/img/user/<?php echo $hasil_profil['foto'];?>">
-                        <span
-                            class="mr-2 d-none d-lg-inline text-gray-600 small ml-2"><?php echo $hasil_profil['nama'];?></span>
+                            src="assets/img/user/<?php echo htmlspecialchars($hasil_profil['foto'] ?? ''); ?>">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small ml-2">
+                            <?php echo htmlspecialchars($hasil_profil['nama'] ?? ''); ?>
+                        </span>
+                        <!-- Badge role -->
+                        <span class="badge badge-<?php echo $role === 'admin' ? 'danger' : 'info'; ?> ml-1">
+                            <?php echo ucfirst($role); ?>
+                        </span>
                         <i class="fas fa-angle-down"></i>
                     </a>
-                    <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                         aria-labelledby="userDropdown">
                         <a class="dropdown-item" href="index.php?page=user">
                             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Profile
+                            Profil
                         </a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="logout.php">
@@ -122,5 +171,5 @@
             </ul>
         </nav>
         <!-- End of Topbar -->
-        <!-- Begin Page Content -->
+
         <div class="container-fluid">
